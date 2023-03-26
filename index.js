cc8e3b4612fa3caff01e0fc907da08e2e1f27042
shon0 <shono.koga@gmail.com>
2023-03-26 22:56:01 +0900

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const { marked } = require('marked')
+const yargs = require('yargs')
+const { hideBin } = require('yargs/helpers')
+const { getPackageName } = require('./lib/name')
+const { readMarkdownFileSync, writeHtmlFileSync } = require('./lib/file')
+
+const { argv } = yargs(hideBin(process.argv))
+  .option('name', {
+    describe: 'CLI名を表示',
+  })
+  .option('file', {
+    describe: 'Markdownファイルのパス',
+  })
+  .option('out', {
+    describe: 'html file',
+    default: 'article.html',
+  })
+
+if (argv.name) {
+  const name = getPackageName()
+  console.log(name)
+  process.exit(0)
+}
+
+const markdownStr = readMarkdownFileSync(path.resolve(__dirname, argv.file))
+const html = marked(markdownStr)
+
+writeHtmlFileSync(path.resolve(__dirname, argv.out), html)
